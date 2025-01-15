7edc40b62cdeef35d33464b6d63fc57823c1548e
HanifRidzwan <hanif_ridzwan@raudah.usim.edu.my>
2025-01-16 03:54:46 +0800

    Update setup_database.py

--- setup_database.py
+++ setup_database.py
@@ -1,4 +1,3 @@
-# setup_database.py
 import sqlite3
 
 # Connect to SQLite database (or create if it doesn't exist)
@@ -12,6 +11,16 @@ CREATE TABLE IF NOT EXISTS users (
     email TEXT UNIQUE NOT NULL,
     staff_id TEXT NOT NULL,
     password TEXT NOT NULL
+)
+''')
+
+# Create the verification_codes table
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS verification_codes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    email TEXT NOT NULL,
+    code TEXT NOT NULL,
+    expiration_time DATETIME NOT NULL
 )
 ''')
 
